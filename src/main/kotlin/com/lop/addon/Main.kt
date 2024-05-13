d103e11d5087d3e715a131652f308c9f9cd7f5c6
Matthias <56599453+12rcu@users.noreply.github.com>
2024-05-13 22:21:59 +0200

    add 2 identical entities with different format versions

--- src/main/kotlin/com/lop/addon/Main.kt
+++ src/main/kotlin/com/lop/addon/Main.kt
@@ -2,68 +2,58 @@ package com.lop.addon
 
 import com.lop.devtools.monstera.addon.addon
 import com.lop.devtools.monstera.addon.dev.zipper.zipWorld
-import com.lop.devtools.monstera.files.File
-import com.lop.devtools.monstera.files.getResource
+import com.lop.devtools.monstera.files.beh.entitiy.components.Components
 import com.lop.devtools.monstera.loadConfig
-import com.lop.devtools.stdlib.furnitures.furniture
-import com.lop.devtools.stdlib.furnitures.shared.FurnitureDropBehaviour
-import com.lop.devtools.stdlib.npcs.npc
-import com.lop.devtools.stdlib.packaging.packaging
-import com.lop.devtools.stdlib.player.Player
-import java.io.File
+import java.awt.Color
 
 fun main(args: Array<String>) {
     val conf = addon(loadConfig().getOrElse {
         it.printStackTrace()
         return
     }) {
-        with(config) {
-            "npm.cmd run build".runCommand(File(System.getProperty("user.dir"), "scripting"))
-            scripts(directory = File("scripting", "dist", "src"))
-            scriptEntryFile = File("scripting", "dist", "src", "index.js")
-        }
-
-        if (args.contains("package")) {
-            packaging {
-                this.world = getResource("world/template-world")
-                addSkinPack {
-                    addSkin(getResource("skin_pack/alex_a.png"), true)
-                    addSkin(getResource("skin_pack/steve_s.png"), true)
-                }
-                addStoreArt { }
-                addMarketing { }
-                addBehaviorPack { }
-                addResourcePack { }
-            }
-        }
-
-        entity("sample_entity", "Sample Entity") {
-            resource {
-                textureLayer(getResource("entities/default_texture.png"))
-                geometryLayer(getResource("entities/default_model.geo.json"))
-            }
+        entity("test_1_19") {
             behaviour {
+                unsafeBehEntityVersion = "1.19.0"
                 components {
-                    physics { }
+                    rideableComponents()
+                }
+            }
+            resource {
+                components {
+                    spawnEgg {
+                        eggByColor(Color.BLUE, Color.CYAN)
+                    }
                 }
             }
         }
-        item("sample_item", "display name") {
-            texture(getResource("item/default_texture.png"))
-        }
-        Player
-            .modify(this)
-            .modifyBehaviour { }
-
-        furniture("vase", "Vase", this) {
-            dropBehaviour = FurnitureDropBehaviour.INVINCIBLE
-        }
-
-        npc("my_npc", "My NPC", this) {
-            //...
+        entity("test_1_20") {
+            behaviour {
+                unsafeBehEntityVersion = "1.20.81"
+                components {
+                    rideableComponents()
+                }
+            }
+            resource {
+                components {
+                    spawnEgg {
+                        eggByColor(Color.GREEN, Color.ORANGE)
+                    }
+                }
+            }
         }
     }
 
     if (args.contains("zip-world"))
         zipWorld(conf, System.getenv("CI_PROJECT_NAME") ?: "local")
+}
+
+fun Components.rideableComponents() {
+    physics { }
+    movement = 0.2
+    inputGroundControlled { }
+    rideable {
+        seat {
+            position(0, 1, 0)
+        }
+    }
 }
